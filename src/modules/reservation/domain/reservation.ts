50c27d57900bfa859bbd54aa1958ba499892d43c
Hanan <hanan@carbonteq.com>
2022-01-24 03:44:19 +0500

    create reservtation entity

---
+++ src/modules/reservation/domain/reservation.ts
@@ -0,0 +1,98 @@
+import { DomainException } from "src/shared/core/DomainException"
+import { Guard } from "src/shared/core/Guard"
+import uuid from "uuid/v4"
+
+interface ReservationProps {
+    firstName: string
+    lastName: string
+    email: string
+    phoneNumber: string
+    streetAddress: string
+    postalCode: string
+    city: string
+    country: string
+    numGuests: number
+    checkInDate: Date
+    checkOutDate: Date
+}
+
+export class Reservation {
+
+    get reservationId(): string {
+        return this.id
+    }
+
+    get firstName(): string {
+        return this.props.firstName
+    }
+
+    get lastName(): string {
+        return this.props.lastName
+    }
+
+    get email(): string {
+        return this.props.email
+    }
+
+    get phoneNumber(): string {
+        return this.props.phoneNumber
+    }
+
+    get streetAddress(): string {
+        return this.props.streetAddress
+    }
+
+    get postalCode(): string {
+        return this.props.postalCode
+    }
+
+    get city(): string {
+        return this.props.city
+    }
+
+    get country(): string {
+        return this.props.country
+    }
+
+    get numGuests(): number {
+        return this.props.numGuests
+    }
+
+    get checkInDate(): Date {
+        return this.props.checkInDate
+    }
+
+    get checkOutDate(): Date {
+        return this.props.checkOutDate
+    }
+
+
+    private constructor(private props: ReservationProps, private id: string = uuid()) { }
+
+    public static create(props: ReservationProps, id?: string) {
+
+        const guardResult = Guard.againstNullOrUndefinedBulk([
+            { argument: props.firstName, argumentName: 'First name' },
+            { argument: props.lastName, argumentName: 'Last name' },
+            { argument: props.email, argumentName: 'Email' },
+            { argument: props.phoneNumber, argumentName: 'Phone number' },
+            { argument: props.streetAddress, argumentName: 'Billing address' },
+            { argument: props.postalCode, argumentName: 'Postal code' },
+            { argument: props.city, argumentName: 'City' },
+            { argument: props.country, argumentName: 'Country' },
+            { argument: props.numGuests, argumentName: 'Number of guests' },
+            { argument: props.checkInDate, argumentName: 'Check-in date' },
+            { argument: props.checkOutDate, argumentName: 'Check-out date' },
+        ])
+
+        if (!guardResult.succeeded) {
+            throw new DomainException(guardResult.message)
+        }
+
+        const reservation = new Reservation({
+            ...props,
+        }, id)
+
+        return reservation;
+    }
+}
---
+++ src/modules/reservation/mappers/reservationMapper.ts
@@ -0,0 +1,69 @@
+import { Reservation } from "../domain/reservation";
+
+interface ReservationDTO {
+    reservationId: string
+    firstName: string
+    lastName: string
+    email: string
+    phoneNumber: string
+    streetAddress: string
+    postalCode: string
+    city: string
+    country: string
+    numGuests: number
+    checkInDate: Date
+    checkOutDate: Date
+}
+
+export class ReservationMap {
+
+    public static toDTO(reservation: Reservation): ReservationDTO {
+        return {
+            reservationId: reservation.reservationId,
+            firstName: reservation.firstName,
+            lastName: reservation.lastName,
+            email: reservation.email,
+            phoneNumber: reservation.phoneNumber,
+            streetAddress: reservation.streetAddress,
+            postalCode: reservation.postalCode,
+            city: reservation.city,
+            country: reservation.country,
+            numGuests: reservation.numGuests,
+            checkInDate: reservation.checkInDate,
+            checkOutDate: reservation.checkOutDate,
+        }
+    }
+
+    public static toDomain(raw: any): Reservation {
+        return Reservation.create({
+            firstName: raw.firstName,
+            lastName: raw.lastName,
+            email: raw.email,
+            phoneNumber: raw.phoneNumber,
+            streetAddress: raw.streetAddress,
+            postalCode: raw.postalCode,
+            city: raw.city,
+            country: raw.country,
+            numGuests: raw.numGuests,
+            checkInDate: raw.checkInDate,
+            checkOutDate: raw.checkOutDate,
+        }, raw.reservationId);
+    }
+
+    public static async toPersistence(reservation: Reservation): Promise<any> {
+        return {
+            reservationId: reservation.reservationId,
+            firstName: reservation.firstName,
+            lastName: reservation.lastName,
+            email: reservation.email,
+            phoneNumber: reservation.phoneNumber,
+            streetAddress: reservation.streetAddress,
+            postalCode: reservation.postalCode,
+            city: reservation.city,
+            country: reservation.country,
+            numGuests: reservation.numGuests,
+            checkInDate: reservation.checkInDate,
+            checkOutDate: reservation.checkOutDate,
+        }
+    }
+}
