0436cfdf9204d35f41761fdb47910e3f1fed9262
Hanan <hanan@carbonteq.com>
2022-01-24 03:15:01 +0500

    feat: add swagger and api versioning

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({ path: "/", version: "v1" })
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get("hello")
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: "api/"
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Lime Home API')
+    .setDescription('API for the Lime Home Reservation System')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
