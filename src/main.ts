1769c55dc1898a26ba502e64273e4f15890a769b
Hanan <hanan@carbonteq.com>
2022-01-26 02:42:15 +0500

    fix: enable cors with origin headers

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule,);
 
   app.enableVersioning({
     type: VersioningType.URI,
@@ -18,6 +18,19 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*",
+    credentials: true
+  });
 
   await app.listen(process.env.PORT || 8080);
 }
